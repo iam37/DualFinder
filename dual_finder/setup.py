2a7ff767ffae271dcdd6978cc01baf467bd6397b
iam37 <isaac.moskowitz@yale.edu>
2024-04-21 22:03:37 -0400

    updated version number and redid PyPI distribution

--- dual_finder/setup.py
+++ dual_finder/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
      name="DualFinder",
-     version="1.0.0",
+     version="1.1.0",
      author="Isaac Moskowitz",
      author_email="isaac.moskowitz@yale.edu",
      description="A trainable and visualizable convolutional neural network designed to detect galaxy and AGN mergers. ",
